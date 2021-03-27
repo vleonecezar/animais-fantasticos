6d5e283e4f3dd5ac93a5f04eea71e866b6fdfa52
Vitor Leone Cezar <vleone.social@gmail.com>
2021-03-27 01:35:02 -0300

    navegação em tabs

---
+++ script.js
@@ -0,0 +1,14 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li')
+const tabContent = document.querySelectorAll('.js-tabcontent section')
+tabContent[0].classList.add('active')
+
+function activeTab(index) {
+  tabContent.forEach(section => {
+    section.classList.remove('active')
+  })
+  tabContent[index].classList.add('active')
+}
+
+tabMenu.forEach((img, index) => {
+  img.addEventListener('click', () => activeTab(index))
+})
