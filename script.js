6ea8550f3980764cbcfe2a9a558f5f53c9927670
Vitor Leone Cezar <vleone.social@gmail.com>
2021-03-27 23:10:55 -0300

    codigo accordion list e variavel active

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
-tabContent[0].classList.add('active')
+function initTabNav() {
+  const activeClass = 'active'
 
-function activeTab(index) {
-  tabContent.forEach(section => {
-    section.classList.remove('active')
+  const tabMenu = document.querySelectorAll('.js-tabmenu li')
+  const tabContent = document.querySelectorAll('.js-tabcontent section')
+  tabContent[0].classList.add(activeClass)
+
+  function activeTab(index) {
+    tabContent.forEach(section => {
+      section.classList.remove(activeClass)
+    })
+    tabContent[index].classList.add(activeClass)
+  }
+  
+  tabMenu.forEach((img, index) => {
+    img.addEventListener('click', () => activeTab(index))
   })
-  tabContent[index].classList.add('active')
-}
 
-tabMenu.forEach((img, index) => {
-  img.addEventListener('click', () => activeTab(index))
-})
+}
+initTabNav()
+
+
+function initAccordion() {
+  const activeClass = 'active'
+
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  accordionList[0].classList.add(activeClass)
+  accordionList[0].nextElementSibling.classList.add(activeClass)
+
+  function activeAccordion() {
+    this.classList.toggle(activeClass)
+    this.nextElementSibling.classList.toggle(activeClass)
+  }
+
+  accordionList.forEach(item => {
+    item.addEventListener('click', activeAccordion)
+  })
+
+}
+initAccordion()
