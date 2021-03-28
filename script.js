afcd9ad2d737fdd0f698903747b62aef8bc7915c
Vitor Leone Cezar <vleone.social@gmail.com>
2021-03-28 02:58:48 -0300

    smooth scroll

--- script.js
+++ script.js
@@ -38,3 +38,22 @@ function initAccordion() {
 
 }
 initAccordion()
+
+function initSmoothScroll() {
+  const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]')
+
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = this.getAttribute('href')
+    const section = document.querySelector(href)
+    section.scrollIntoView({
+      block: 'start',
+     behavior: 'smooth'
+    })
+  }
+
+  internalLinks.forEach(link => {
+    link.addEventListener('click', scrollToSection)
+  })
+}
+initSmoothScroll()
