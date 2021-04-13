81dac8bd8b616df496f24c43eb02cad5a9124f75
Vitor Leone Cezar <vleone.social@gmail.com>
2021-04-13 16:34:37 -0300

    arquivo de importação js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initSmoothScroll from './modules/smooth-scroll.js'
 import initAnimeScroll from './modules/anime-scroll.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
+import initModal from './modules/modal.js'
 
 initSmoothScroll()
 initAnimeScroll()
 initAccordion()
 initTabNav()
+initModal()
