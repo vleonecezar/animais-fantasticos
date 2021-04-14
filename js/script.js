b45b93527cb55cf6de2ada46bd9c04d7390dc711
Vitor Leone Cezar <vleone.social@gmail.com>
2021-04-14 12:30:22 -0300

    importação do tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimeScroll from './modules/anime-scroll.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initSmoothScroll()
 initAnimeScroll()
 initAccordion()
 initTabNav()
 initModal()
+initTooltip()
