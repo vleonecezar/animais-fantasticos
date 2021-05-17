cd9f8c0e06185729404a066cc0cfd57656afd555
Vitor Leone Cezar <vleone.social@gmail.com>
2021-05-17 20:28:06 -0300

    correções com eslint

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,31 +1,33 @@
 export default function initAnimaNumeros() {
   function animaNumeros() {
-    const numeros = document.querySelectorAll('[data-numero]')
+    const numeros = document.querySelectorAll('[data-numero]');
 
-    numeros.forEach(numero => {
-      const total = +numero.innerText
-      const incremento = Math.floor(total / 100)
-      let start = 0
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const incremento = Math.floor(total / 100);
+      let start = 0;
       const timer = setInterval(() => {
-        start += incremento
-        numero.innerText = start
+        start += incremento;
+        numero.innerText = start;
         if (start > total) {
-          numero.innerText = total
-          clearInterval(timer)
+          numero.innerText = total;
+          clearInterval(timer);
         }
-      }, 25 * Math.random())
-    })
+      }, 25 * Math.random());
+    });
   }
+
+  let observer;
 
   function handleMutation(mutation) {
     if (mutation[0].target.classList.contains('ativo')) {
-      observer.disconnect()
-      animaNumeros()
+      observer.disconnect();
+      animaNumeros();
     }
   }
 
-  const observerTarget = document.querySelector('.numeros')
-  const observer = new MutationObserver(handleMutation)
+  observer = new MutationObserver(handleMutation);
 
-  observer.observe(observerTarget, { attributes: true })
+  const observerTarget = document.querySelector('.numeros');
+  observer.observe(observerTarget, { attributes: true });
 }
